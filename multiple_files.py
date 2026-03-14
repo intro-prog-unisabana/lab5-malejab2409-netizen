a2fb70bc2a7336c2a45c7f7517b4cf5a7fd8f2d3
Maria Alejandra <malejab2409@gmail.com>
2026-03-13 23:28:53 -0500

    Problema 6

---
+++ multiple_files.py
@@ -0,0 +1,8 @@
+from utils import count_letters, flip, greet 
+flip 
+count_letters
+entrada = input ("Please type your message\n")
+invertido = entrada [::-1]
+ivertidoo = invertido+"1"
+
+print(f"Your encoded message is: {invertido}1 ")
